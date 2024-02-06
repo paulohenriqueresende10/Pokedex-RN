8f2e329208e2c13fa7876cc923045602faf89366
paulohenriqueresende10 <paulo-henrique_1993@hotmail.com>
2024-02-05 23:35:53 -0300

    first commit

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,55 @@
+import { Dimensions } from 'react-native';
+import styled, { css } from 'styled-components/native';
+
+const windowWidth = Dimensions.get('window').width;
+
+export const LoadingScreen = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Container = styled.View`
+  ${({ theme }) => css`
+    background: ${theme.colors.background};
+    flex: 1;
+
+    position: relative;
+  `}
+`;
+
+export const Header = styled.ImageBackground`
+  ${({ theme }) => css`
+    width: ${windowWidth}px;
+    margin-left: -20px;
+    height: 220px;
+    background: ${theme.colors.background};
+  `}
+`;
+
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.colors.text};
+    font-size: 32px;
+    line-height: 38px;
+    font-weight: bold;
+    margin-top: -70px;
+  `}
+`;
+
+export const Text = styled.Text`
+${({ theme }) => css`
+    color: ${theme.colors.text};
+    font-size: 16px;
+    line-height: 19px;
+    font-weight: 400;
+  `}
+`;
+
+export const Input = styled.TextInput`
+  ${({ theme }) => css`
+      color: ${theme.colors.text};
+      border-radius: 10px
+    `}
+`;
+
---
+++ src/pages/Welcome/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+
+
+import AnimatedLottieView from 'lottie-react-native';
+
+import pokemonAnimation from './pokemon.json';
+
+import * as S from './styles';
+
+type RootStackParamList = {
+  Home: undefined;
+};
+
+export function Welcome() {
+  const { navigate } = useNavigation<StackNavigationProp<RootStackParamList>>();
+
+  function handleNavigateToHome() {
+    navigate('Home');
+  }
+
+  return (
+    <S.Container>
+      <S.Content>
+        <S.WrapperIcon>
+          <S.IconContent>
+            <AnimatedLottieView autoPlay source={pokemonAnimation} loop />
+          </S.IconContent>
+        </S.WrapperIcon>
+
+        <S.Title>Pokédex</S.Title>
+        <S.SubTitle>Encontre todos os pokémons em um só lugar</S.SubTitle>
+      </S.Content>
+
+      <S.Bottom>
+        <S.Button onPress={handleNavigateToHome}>
+          <S.ButtonText>Entrar</S.ButtonText>
+        </S.Button>
+      </S.Bottom>
+    </S.Container>
+  );
+}
